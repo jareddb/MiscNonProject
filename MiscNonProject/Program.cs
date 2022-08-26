cd33b518ab47d041c82dea721637342e7365583c
jared <jared.brinton@gmail.com>
2022-08-26 15:43:18 -0700

    Git Init

---
+++ MiscNonProject/Program.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+//using DataStructuresI;
+
+namespace MiscNonProjectConsole{
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            var host = CreateDefaultBuilder().Build();
+
+            using IServiceScope serviceScope = host.Services.CreateScope();
+            IServiceProvider provider = serviceScope.ServiceProvider;
+            var executor = provider.GetRequiredService<Executor>();
+
+            executor.ExecuteCommand(args);
+
+            host.Run();
+        }
+
+        static IHostBuilder CreateDefaultBuilder() {
+            return Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration(app => {
+                    var settings = app.AddJsonFile("appsettings.json").Build();
+                })
+                .ConfigureLogging((hostingContext, loggingBuilder) => {
+                    //loggingBuilder.ClearProviders();
+                    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("logging"));
+                    loggingBuilder.AddSimpleConsole(options => {
+                        options.IncludeScopes = true;
+                    });
+                    loggingBuilder.AddEventLog();
+                    loggingBuilder.AddDebug();
+                })
+                .ConfigureServices(services => {
+                    services.AddLogging();
+                    services.AddTransient<Executor>();
+                });
+            
+        }
+
+    }
+
+    internal class Executor {
+
+        IConfiguration _config;
+        ILogger<Executor> _logger;
+
+        public Executor(IConfiguration config, ILogger<Executor> logger) {
+            _config = config;
+            _logger = logger;
+        }
+
+        public void ExecuteCommand(string[] args) {
+            Console.WriteLine("");
+
+            if(args.Length != 3) {
+                _logger.LogError($"You must pass in three parameters, the assembly name, the namespace name and the class name");
+                throw new ArgumentException();
+            }
+
+            var assName = args[0];
+            var namesp = args[1];
+            var className = args[2];
+
+            _logger.LogInformation($"Running Tests For:{Environment.NewLine}{Environment.NewLine}" +
+                $"Assembly:  {assName}{Environment.NewLine}" +
+                $"Namespace: {namesp}{Environment.NewLine}" +
+                $"Class:     {className}{Environment.NewLine}");
+
+            //Assembly.GetAssembly("ContainsDuplicate");
+
+            Assembly assembly = Assembly.Load(assName);
+            Type type = assembly.GetType($"{namesp}.{className}");
+            if(type != null) {
+                object scenarioObj = Activator.CreateInstance(type);
+                MethodInfo method = type.GetMethod("RunScenarios");
+                method.Invoke(scenarioObj, new object[] {_config, _logger});
+            } else { _logger.LogError($"ERROR: Invalid Namespace or Class Name"); throw new ArgumentException(); }
+
+            _logger.LogInformation($"Selected scenario successfully run{Environment.NewLine}");
+
+            System.Threading.Thread.Sleep(500);
+            System.Environment.Exit(0);
+        }
+
+    }
+
+
+}
---
+++ PracticeProblems/DataStructuresI.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace DataStructuresI {
+
+    #region Template
+
+    /// <summary>
+    /// Template Class for a LeetCode problem. Use the below formatting to create your solution, test it against a number of inputs and output results to the console. 
+    /// Put URL to problem in below summary line:
+    /// https://leetcode.com/problems/template_problem
+    /// </summary>
+    public class Template {
+
+        List<Inputs> inputs;
+
+        private class Inputs {
+            public int[] Nums { get; set; }
+            public int Target { get; set; }
+        }
+
+        public Template() {
+            inputs = new List<Inputs>();
+            inputs.Add(new Inputs() { Nums = new[] { 2, 7, 11, 15 }, Target = 9 });
+            inputs.Add(new Inputs() { Nums = new[] { 3, 2, 4 }, Target = 6 });
+            inputs.Add(new Inputs() { Nums = new[] { 3,3 }, Target = 6 });
+        }
+
+        public void RunScenarios(IConfiguration config, ILogger logger) {
+            List<string> result = new List<string>();
+            foreach (var input in inputs) {
+                result.Add(DoWork(input).ToString());
+            }
+            logger.LogInformation($"Test Results:{Environment.NewLine}{string.Join($"{Environment.NewLine}", result)}{Environment.NewLine}");
+        }
+
+        private bool DoWork(Inputs input) {
+            return true;
+        }
+    }
+
+    #endregion
+
+
+    #region ContainsDuplicate
+
+    /// <summary>
+    /// For an inputted int array, returns a bool true if there are any duplicated integers in the list 
+    /// https://leetcode.com/problems/contains-duplicate
+    /// </summary>
+    public class ContainsDuplicate {
+
+        List<int[]> inputs;
+        public ContainsDuplicate() {
+            inputs = new List<int[]>();
+            inputs.Add(new[] { 0,1,2,0 });
+            inputs.Add(new[] { 1,2,3,4 });
+            inputs.Add(new[] { -1, -1, 10, 10 });
+        }
+
+        public void RunScenarios(IConfiguration config, ILogger logger) {
+            List<string> result = new List<string>();
+            foreach (var input in inputs) {
+                result.Add(DoWork(input).ToString());
+            }
+            logger.LogInformation($"Test Results:{Environment.NewLine}{string.Join($"{Environment.NewLine}", result)}{Environment.NewLine}");
+        }
+
+        private bool DoWork(int[] nums) {
+            var dupes = nums.GroupBy(x => x).Where(x => x.Count() > 1);
+            bool distinct = dupes.Count() < 1 ? false : true;
+            return distinct;
+        }
+    }
+    #endregion
+
+}
